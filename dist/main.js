18c32f162313c4a75eee88c17aa9ee2d6e542a67
Hero <heroemenalom73@gmail.com>
2024-04-28 11:01:55 -0400

    add boilerplate for bringing bot online

--- dist/main.js
+++ dist/main.js
@@ -1,2 +1,14 @@
 "use strict";
-console.log('test');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const discord_js_1 = require("discord.js");
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const token = process.env.TOKEN;
+const client = new discord_js_1.Client({ intents: [] });
+client.once('ready', client => {
+    console.log(`${client.user.username} is Online!`);
+});
+client.login(token);
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,14 @@
-console.log('test')
+import { Client } from 'discord.js'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const token: string = process.env.TOKEN!
+
+const client = new Client({ intents: [] })
+
+client.once('ready', client => {
+    console.log(`${client.user.username} is Online!`)
+})
+
+
+client.login(token)
